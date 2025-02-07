4c37fe6ab39351a68d2e6828ac7e14b029c9eb88
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2025-02-07 15:24:12 +0530

    sign in and sign up backend completed for sqlite

--- app/src/main/java/lk/evenro/even/SignInActivity.java
+++ app/src/main/java/lk/evenro/even/SignInActivity.java
@@ -1,6 +1,13 @@
 package lk.evenro.even;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +27,65 @@ public class SignInActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText email = findViewById(R.id.sign_in_email);
+        EditText password = findViewById(R.id.sign_in_password);
+
+        Button signin = findViewById(R.id.sign_in_button);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (email.getText().toString().isEmpty()) {
+                    Log.i("Sign In", "Invalid Email");
+                } else if (password.getText().toString().isEmpty()) {
+                    Log.i("Sign In", "Invalid Password");
+                } else {
+                    UserDataBase userData = new UserDataBase(getApplicationContext(), "evenro.dp", null, 1);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Cursor cursor = userData.getReadableDatabase().query("user", null, null, null, null, null, null);
+
+                            while (cursor.moveToNext()) {
+                                String resultEmail = cursor.getString(2);
+                                String resultPassword = cursor.getString(4);
+                                Log.i("SIGN UP", resultEmail);
+
+                                if(resultEmail.equals(email.getText().toString()) && resultPassword.equals(password.getText().toString())){
+                                    Log.i("SIGN UP", "Email already Use");
+
+                                    email.setText("");
+                                    password.setText("");
+
+                                }else{
+
+                                    Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                                    startActivity(intent);
+
+                                }
+
+                            }
+
+
+                        }
+                    }).start();
+
+
+                }
+            }
+
+        });
+
+        TextView text = findViewById(R.id.sign_in_text);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/lk/evenro/even/SignUpActivity.java
+++ app/src/main/java/lk/evenro/even/SignUpActivity.java
@@ -1,13 +1,26 @@
 package lk.evenro.even;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import lk.evenro.even.validation.Validation;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -21,6 +34,110 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button sign_up_button = findViewById(R.id.sign_up_button);
+        EditText full_name = findViewById(R.id.sign_up_full_name);
+        EditText email = findViewById(R.id.sign_up_email);
+        EditText password = findViewById(R.id.sign_up_password);
+        EditText confrom_password = findViewById(R.id.sign_up_confrom_password);
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (full_name.getText().toString().isEmpty()) {
+                    Log.i("SIGN UP", "Enter you Full Name");
+                } else if (email.getText().toString().isEmpty()) {
+                    Log.i("SIGN UP", "Enter you Email");
+                } else if (!Validation.isEmailValid(email.getText().toString())) {
+                    Log.i("SIGN UP", "Invalid you Email");
+                } else if (password.getText().toString().isEmpty()) {
+                    Log.i("SIGN UP", "Enter you password");
+                } else if (!Validation.isPasswordValid(password.getText().toString())) {
+                    Log.i("SIGN UP", "Invalid you password");
+                } else if (password.getText().toString().matches(confrom_password.getText().toString())) {
+                    Log.i("SIGN UP", "This not match");
+                } else {
+
+                    UserDataBase userData = new UserDataBase(getApplicationContext(), "evenro.dp", null, 1);
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            int code  = (int) (Math.random() * 1000);
+
+                            Cursor cursor = userData.getReadableDatabase().query("user", null, null, null, null, null, null);
+
+                            while (cursor.moveToNext()) {
+                                String resultEmail = cursor.getString(2);
+                                Log.i("SIGN UP", resultEmail);
+
+                                if(resultEmail.equals(email.getText().toString())){
+                                    Log.i("SIGN UP", "Email already Use");
+                                }else{
+
+                                    ContentValues contentValues = new ContentValues();
+                                    contentValues.put("name", full_name.getText().toString());
+                                    contentValues.put("email", email.getText().toString());
+                                    contentValues.put("password", confrom_password.getText().toString());
+                                    contentValues.put("verification", code);
+
+                                    long id = userData.getWritableDatabase().insert("user", null, contentValues);
+                                    Log.i("SIGN UP", String.valueOf(id));
+
+                                    full_name.setText("");
+                                    email.setText("");
+                                    password.setText("");
+                                    confrom_password.setText("");
+
+                                    Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+                                    startActivity(intent);
+
+                                }
+
+                            }
+
+                        }
+                    }).start();
+                }
+            }
+        });
+
+        TextView sing_in_text = findViewById(R.id.sign_up_text);
+        sing_in_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+
+class UserDataBase extends SQLiteOpenHelper {
+
+    public UserDataBase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE user (\n" +
+                "    id           INTEGER PRIMARY KEY\n" +
+                "                         NOT NULL,\n" +
+                "    name         TEXT    NOT NULL,\n" +
+                "    email        TEXT    NOT NULL,\n" +
+                "    mobile       TEXT,\n" +
+                "    password     TEXT    NOT NULL,\n" +
+                "    gender       TEXT,\n" +
+                "    verification TEXT\n" +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 }
