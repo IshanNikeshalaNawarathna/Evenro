e1da84fd2c0f88e7ed414af04d733efb37e4d3bb
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2025-02-09 02:40:19 +0530

    bug fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation ("com.google.android.material:material:1.6.0")
+
 }
--- app/src/main/java/lk/evenro/even/EventAddActivity.java
+++ app/src/main/java/lk/evenro/even/EventAddActivity.java
@@ -2,17 +2,23 @@ package lk.evenro.even;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +26,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+//import com.google.android.gms.tasks.OnFailureListener;
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.firebase.firestore.DocumentReference;
+//import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import lk.evenro.even.model.SpinnerItem;
@@ -30,6 +42,7 @@ public class EventAddActivity extends AppCompatActivity {
     TextView category;
     String categ;
     String imageUrl;
+    private ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +72,26 @@ public class EventAddActivity extends AppCompatActivity {
                 timeBottomSheetFragment.show(getSupportFragmentManager(), timeBottomSheetFragment.getTag());
             }
         });
+        ImageView imageView = findViewById(R.id.event_img_view);
+        pickMedia = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+            if (uri != null) {
+                imageView.setImageURI(uri);
+                imageUrl = uri.toString();
+                Log.i("ABC", imageUrl);
+            } else {
+                Log.d("PhotoPicker", "No media selected");
+            }
+        });
+        ImageButton selectImge = findViewById(R.id.image_select_button);
+        selectImge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pickMedia.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                        .build());
+            }
+        });
+
 
         Spinner spinner = findViewById(R.id.event_category_item);
 
@@ -83,9 +116,91 @@ public class EventAddActivity extends AppCompatActivity {
             }
         });
 
+        Button add_event_button = findViewById(R.id.event_add_button);
+        EditText event_name = findViewById(R.id.add_event_name);
+        EditText event_time = findViewById(R.id.add_event_time);
+        EditText event_date = findViewById(R.id.add_event_date);
+        EditText event_price = findViewById(R.id.add_event_price);
+        EditText event_qty = findViewById(R.id.add_event_qty);
+        EditText event_location = findViewById(R.id.add_event_location);
+        EditText event_description = findViewById(R.id.add_event_description);
+
+
+        add_event_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (imageUrl == null) {
+                    Log.i("EVENT ADD", "Select an Image");
+                } else if (event_name.getText().toString().trim().isEmpty()) {
+                    Log.i("EVENT ADD", "Type an Event name");
+                } else if (event_time.getText().toString().trim().isEmpty()) {
+                    Log.i("EVENT ADD", "Choose your Time");
+                } else if (event_date.getText().toString().trim().isEmpty()) {
+                    Log.i("EVENT ADD", "Choose your Date");
+                } else if (event_price.getText().toString().trim().isEmpty()) {
+                    Log.i("EVENT ADD", "Type a Ticket Price");
+                } else if (event_qty.getText().toString().trim().isEmpty()) {
+                    Log.i("EVENT ADD", "Type a Ticket Quantity");
+                } else if (spinner.getSelectedItem() == null) {
+                    Log.i("EVENT ADD", "Select an Event Category");
+                } else if (event_location.getText().toString().trim().isEmpty()) {
+                    Log.i("EVENT ADD", "Type a Location");
+                } else if (event_description.getText().toString().trim().isEmpty()) {
+                    Log.i("EVENT ADD", "Type an Event Description");
+                } else {
+
+                    String eventName = event_name.getText().toString().trim();
+                    String eventTime = event_time.getText().toString().trim();
+                    String eventDate = event_date.getText().toString().trim();
+                    String eventPrice = event_price.getText().toString().trim();
+                    String eventQty = event_qty.getText().toString().trim();
+                    String eventCategory = String.valueOf(categ);
+                    String eventLocation = event_location.getText().toString().trim();
+                    String eventDescription = event_description.getText().toString().trim();
+
+                    // FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+                    HashMap<String, Object> event_data = new HashMap<>();
+                    event_data.put("event_name", eventName);
+                    event_data.put("organizer_name", "Ishan Nikeshala Nawarathna");
+                    event_data.put("qty", eventQty);
+                    event_data.put("event_date", eventDate);
+                    event_data.put("event_time", eventTime);
+                    event_data.put("price", "Rs." + eventPrice + ".00");
+                    event_data.put("event_category", eventCategory);
+                    event_data.put("event_location", eventLocation);
+                    event_data.put("event_description", eventDescription);
+                    event_data.put("event_image", imageUrl);
+
+//                    firebaseFirestore.collection("event").add(event_data).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                        @Override
+//                        public void onSuccess(DocumentReference documentReference) {
+//                            Log.i("EVENT ADD", documentReference.getId());
+//
+//                        }
+//                    }).addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception e) {
+//                            Log.i("EVENT ADD", e.toString());
+//                        }
+//                    });
+
+                    event_name.setText("");
+                    event_date.setText("");
+                    event_time.setText("");
+                    event_qty.setText("");
+                    event_price.setText("");
+                    event_location.setText("");
+                    event_description.setText("");
+                    spinner.setSelection(0);
+                }
+            }
+        });
 
     }
+
 }
+
 class SpinnerAdapter extends ArrayAdapter<SpinnerItem> {
 
     List<SpinnerItem> spinnerItems;
