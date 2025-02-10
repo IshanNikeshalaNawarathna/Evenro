e02cc3879a9ef4f0a206127469b572ef3270f03c
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2025-02-11 02:28:03 +0530

    code update

--- app/src/main/java/lk/evenro/even/SearchActivity.java
+++ app/src/main/java/lk/evenro/even/SearchActivity.java
@@ -1,8 +1,10 @@
 package lk.evenro.even;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,12 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import lk.evenro.even.adapter.EventAdapter;
 import lk.evenro.even.model.EventDetails;
 
 public class SearchActivity extends AppCompatActivity {
+
+    ;
+    ArrayList<EventDetails> details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +46,26 @@ public class SearchActivity extends AppCompatActivity {
         });
 
         ArrayList<EventDetails> data = new ArrayList<>();
-        data.add(new EventDetails("Music","ABC","hello","200","hello","10","2025-02-01","8.00am"));
+        data.add(new EventDetails("Music", "ABC", "hello", "200", "hello", "10", "2025-02-01", "8.00am"));
+
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("event").document("hQl8EwyImP6kzq3QU5Vb").get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            DocumentSnapshot document = task.getResult();
+
+
+                        }
+                    }
+                });
 
 
         RecyclerView recyclerView = findViewById(R.id.search_item_recycle_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(new EventAdapter(data));
 
     }
