464ba170cafa36d57a6f4bab4ee805a875ccab85
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2025-02-12 16:56:07 +0530

    navigation to event cart activity and pass the details

--- app/src/main/java/lk/evenro/even/DetailEventActivity.java
+++ app/src/main/java/lk/evenro/even/DetailEventActivity.java
@@ -1,5 +1,6 @@
 package lk.evenro.even;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -49,7 +50,9 @@ public class DetailEventActivity extends AppCompatActivity {
         buy_ticket_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getApplicationContext(),EventCartActivity.class);
+                intent.putExtra("cart_details",details);
+                startActivity(intent);
             }
         });
 
