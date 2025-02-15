3bb4a6510ac3bc65a8b11cd1a954bccc63d782f2
Ishan Nikeshala <ishannikeshala1999@gmail.com>
2025-02-16 02:15:47 +0530

    map loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation ("com.google.android.material:material:1.6.0")
     implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
 
 
     implementation ("com.github.PayHereDevs:payhere-android-sdk:v3.0.17")
